1e19c81c6c40f6900a9f3aa7b2ecf9a80d1534ae
ShamanUX <petrus.eskelinen@protonmail.com>
2025-11-21 18:11:38 +0200

    fix: add frames to project sections

--- app/page.tsx
+++ app/page.tsx
@@ -70,23 +70,76 @@ function Header() {
   );
 }
 
+const PartialBorder: React.FC<{ side: string; flip?: boolean }> = ({ side, flip }) => {
+  const height = side === "left" || side === "right" ? "h-[80px]" : "h-[2px]";
+  const width = side === "top" || side === "bottom" ? "w-[80px]" : "w-[2px]";
+  let secondSide = "";
+  if (side === "left") {
+    secondSide = flip ? "bottom" : "top";
+  } else if (side === "right") {
+    secondSide = flip ? "top" : "bottom";
+  } else if (side === "top") {
+    secondSide = flip ? "right" : "left";
+  } else if (side === "bottom") {
+    secondSide = flip ? "left" : "right";
+  }
+
+  // Set style variable based on selected side
+  const style: React.CSSProperties = {};
+  const shiftAmount = "16px";
+  if (side === "left") style.left = shiftAmount;
+  if (side === "right") style.right = shiftAmount;
+  if (side === "top") style.top = shiftAmount;
+  if (side === "bottom") style.bottom = shiftAmount;
+  if (secondSide === "left") style.left = shiftAmount;
+  if (secondSide === "right") style.right = shiftAmount;
+  if (secondSide === "top") style.top = shiftAmount;
+  if (secondSide === "bottom") style.bottom = shiftAmount;
+
+  return <div className={`absolute bg-highlight ${height} ${width} shadow-sm shadow-highlight`} style={style} />;
+};
+
 interface ProjectCardProps {
   name: string;
   role: string;
   imgUrl: string;
   imgAlt: string;
   tech?: string[];
+  flipBorder?: boolean;
 }
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ name, role, imgUrl, imgAlt, tech }) => {
+const ProjectCard: React.FC<ProjectCardProps> = ({ name, role, imgUrl, imgAlt, tech, flipBorder }) => {
   return (
-    <div className="flex flex-col min-w-full p-4 rounded-xl">
-      <h3 className="text-2xl"> {name} </h3>
-      <div className="bg-amber-400 w-fit p-2">
-        <p>{role}</p>
-        <p> {tech && tech?.join(", ")}</p>
+    <div className="flex flex-col rounded-sm min-w-full p-8 relative shadow-sm shadow-highlight">
+      <div className="flex justify-between min-h-[60px]">
+        <div className="flex flex-col gap-2">
+          <h3 className="text-2xl align-top leading-[1]"> {name} </h3>
+          <p>{role}</p>
+        </div>
+
+        <div className="max-w-1/3 font-mono lowercase text-sm flex flex-wrap flex-row-reverse items-center gap-y-1 gap-4 -mt-2">
+          {tech &&
+            tech.map((t, i) => (
+              <span key={t} className="flex items-center gap-1">
+                <svg
+                  width="6"
+                  height="6"
+                  viewBox="0 0 10 10"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="inline-block rotate-90 opacity-60"
+                >
+                  <polygon points="5,0 10,10 0,10" />
+                </svg>
+                {t}
+              </span>
+            ))}
+        </div>
       </div>
       <Image className="min-w-full" width={1000} height={1000} src={imgUrl} alt={imgAlt}></Image>
+      <PartialBorder side="right" flip />
+      <PartialBorder side="top" />
+      <PartialBorder side="left" />
     </div>
   );
 };
@@ -134,11 +187,11 @@ export default function Home() {
         <Header />
         <div className="flex flex-col w-full px-4 sm:px-16 max-w-4xl">
           <SectionHeading id="projects" name="Featured projects" />
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-16">
             <ProjectCard
               name={"Easybook"}
               role={"Full Stack Developer, UX Designer"}
-              tech={["React", "Node.js", "Tailwind", "Vercel", "Firestore db", "Cypress", "Figma"]}
+              tech={["React", "Node.js", "Tailwind", "Vercel", "Firestore", "Cypress", "Figma"]}
               imgUrl={"/projectImages/EasybookShowcase.png"}
               imgAlt={"Easybook UX designs"}
             />
@@ -168,7 +221,7 @@ export default function Home() {
             companyName="Easybook"
             timeRange="Nov 2024 - Present"
             roles={["Full Stack Developer", "UX Designer"]}
-            tech={["React", "Node.js", "Tailwind", "Vercel", "Firestore db", "Cypress", "Figma"]}
+            tech={["React", "Node.js", "Tailwind", "Vercel", "Firestore", "Cypress", "Figma"]}
             description="Working for a SaaS startup, developed a robust embeddable calendar/chatbot system with React, Node.js and Typescript, utilizing Vercel and Cloud Firestore. I was responsible for designing the visual look of the app and new features. Implemented automated tests with Cypress. Took charge of developing the startup's devops and improved the way of working, both in terms of coding best practices and communication."
           />
           <h2 className="text-3xl" id="references"></h2>

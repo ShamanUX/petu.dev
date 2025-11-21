d74edeb0f82e733e1f71589884fbd13187bc00f6
ShamanUX <petrus.eskelinen@protonmail.com>
2025-11-21 22:11:32 +0200

    Updates

--- _next/static/huKtwPbokgUMbZfNANqsi/_buildManifest.js
+++ _next/static/huKtwPbokgUMbZfNANqsi/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/your.repo.name//_next/:path+",
+        "destination": "/your.repo.name/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [

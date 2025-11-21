80fbf5c55546ca5c72f6c471921dad2e8969a202
ShamanUX <petrus.eskelinen@protonmail.com>
2025-11-21 22:12:53 +0200

    Updates

--- _next/static/6nqVluJtt4UjFfW-ksjpA/_buildManifest.js
+++ _next/static/6nqVluJtt4UjFfW-ksjpA/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/your.repo.name//_next/:path+",
-        "destination": "/your.repo.name/_next/:path+"
+        "source": "/petu.dev//_next/:path+",
+        "destination": "/petu.dev/_next/:path+"
       }
     ],
     "fallback": []
